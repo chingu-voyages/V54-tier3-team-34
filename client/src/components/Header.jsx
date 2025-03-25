2593b4989407bb9917422fb795c17ef312f96e61
Greg <gregminezzi@gmail.com>
2025-03-25 18:14:49 +0100

    add onclick to tooltip for mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
       <h1 className="mt-10 text-center text-4xl text-green-500 drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
         Penta AI
       </h1>
-      <div className="text-xs text-green-500">{formattedDate}</div>
+      <div className="text-xs text-white">{formattedDate}</div>
     </div>
   );
 }
