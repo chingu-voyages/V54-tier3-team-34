ed07a9d4afdcc1cbdf5db5d57582f973899bc78c
Greg <gregminezzi@gmail.com>
2025-03-25 16:05:47 +0100

    add clear/continue buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,14 @@ function App() {
             >
               Persona
             </label>
-  
+            <div className="absolute bottom-5 flex w-full items-center justify-around gap-3">
+              <button className="bg-green-500 px-3 py-1 shadow-[3px_3px_white] hover:translate-x-[1px] hover:translate-y-[1px] hover:shadow-[1px_1px_white] active:translate-x-[2px] active:translate-y-[2px] active:shadow-[0px_0px_white]">
+                clear
+              </button>
+              <button className="bg-green-500 px-3 py-1 shadow-[3px_3px_white] hover:translate-x-[1px] hover:translate-y-[1px] hover:shadow-[1px_1px_white] active:translate-x-[2px] active:translate-y-[2px] active:shadow-[0px_0px_white]">
+                continue
+              </button>
+            </div>
           
           </div>
 
