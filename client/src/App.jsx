dfb096025e170ffa3aa5c5d926c354e2d25b4701
Greg <gregminezzi@gmail.com>
2025-03-25 16:04:26 +0100

    initial input form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,41 @@
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="font-tektur text-4xl text-green-500 drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
-        Welcome to AI Helper
-      </h1>
+    <div class="flex min-h-screen flex-col items-center gap-5 bg-black">
+        <div className="flex flex-col items-center justify-center">
+          <h1 class="mt-10 text-center text-4xl text-green-500 drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
+            Penta AI
+          </h1>
+          <div className="text-xs text-green-500">March 24, 2025</div>
+        </div>
+{/* header */}
+<div className="w-full max-w-[1000px] flex-1 border border-green-500">
+<div className="bg-green-500 p-3">
+            A <strong>persona</strong> describes who the result will be tailored
+            for
+          </div>
+{/* input area */}
+<div class="relative mx-1 mt-5 p-1">
+            <textarea
+              class="peer field-sizing-content min-h-52 w-full border border-green-500 p-3 pt-8 pb-12 text-green-500 outline-offset-3 valid:pt-2 valid:pb-2 valid:outline valid:outline-green-500 focus:pt-2 focus:pb-18 focus:outline focus:outline-green-500"
+              placeholder="You are a Product Owner, Scrum Master, UI/UX Designer, Web Developer, or
+  Data Scientist who is at the beginning of your career and is looking to apply
+  what you've learned to build practical experience to help you get noticed in
+  the job market."
+              required
+            ></textarea>
+            <label
+              for="name"
+              class="pointer-events-none absolute top-0 left-1 m-2 bg-black px-1 text-green-500 transition-all duration-400 peer-valid:-top-[15px] peer-valid:bg-green-500 peer-valid:text-xs peer-valid:text-white peer-focus:-top-[15px] peer-focus:bg-green-500 peer-focus:text-xs peer-focus:text-white"
+            >
+              Persona
+            </label>
+  
+          
+          </div>
+
+
+</div>
+
     </div>
   );
 }
