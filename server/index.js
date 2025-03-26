03a0d71b44d44f459e57757413c5e47d52564d69
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-03-26 07:32:05 +0000

    ops: serve frontend files from the server when deployed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
+import express from "express";
 import { createServer } from "node:http";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// allows BE and FE to be deployed together
+// by serving the client's dist files using our server
+if (process.env.NODE_ENV == "production") {
+  const staticDir = fileURLToPath(new URL("../client/dist", import.meta.url));
+  app.use(express.static(staticDir));
+
+  app.get("*", function serveStaticFiles(_, res) {
+    res.sendFile(path.join(staticDir, "index.html"));
+  });
+}
+
 import app from "./src/app.js";
 
 const PORT = process.env.PORT;
