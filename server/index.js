6b065493bc8b06da0d9d342486e59d3d152583ff
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-03-26 07:14:18 +0000

    ops(server): support env vars

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import { createServer } from "node:http";
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+if (PORT === undefined) {
+  throw new Error(
+    "error finding PORT in env vars. setup .env.local following the documentation inside .env",
+  );
+}
+
 createServer(app).listen(PORT, () => {
   console.info(`server has started listening on port ${PORT}`);
 });
