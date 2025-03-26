308d3e0ff48fd90e51b2c9dc4c584cd292e27c5c
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-03-26 11:42:46 -0300

    Create AiService component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
+import { AiService } from "./AiService.jsx";
+
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black">
       <h1 className="font-tektur text-4xl text-green-500 drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
         Welcome to AI Helper
       </h1>
+      <AiService/>
     </div>
   );
-}
+} 
 
 export default App;
