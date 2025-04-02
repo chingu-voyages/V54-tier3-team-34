58edfadb7a5cd4c8c0c2630238c2100fa4b097c6
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-04-02 10:08:13 -0300

    refactor: replace AiService with call to ai-model module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,9 @@ import Header from "./components/Header";
 import TextArea from "./components/TextArea";
 import Footer from "./components/Footer";
 import ProgressBar from "./components/ProgressBar";
-import { AiService } from "./components/AiService/AiService";
 import OutputField from "./components/OutputField";
 import { steps } from "./steps";
-import { GoogleGenAI } from "@google/genai";
+import { generateWithGemini } from "./ai-model";
 
 
 function App() {
@@ -20,9 +19,8 @@ function App() {
   });
   const [stepNumber, setStepNumber] = useState(0);
   const [currentStep, setCurrentStep] = useState(steps[0]);
-  const [isSubmitted, setIsSubmitted] = useState(false);
   const [aiResponse, setAiResponse] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
   const [errorMessages, setErrorMessages] = useState({});
 
   // change currentStep everytime stepNumber changes
@@ -69,7 +67,7 @@ function App() {
   };
 
   // handle submit form
-  const handleSubmit = (e) => {
+  const handleSubmit = async () => {
     e.preventDefault();
     // run validation function.  if error string is returned, set errorMessage and stop and return
     const missingData = validateInput();
@@ -80,18 +78,17 @@ function App() {
       return;
     }
     // if no errors - proceed to communicate with API
-    console.log("Submitted success - communicate with API HERE");
-  };
-
-  const geminiApiHandler = async (formData) => {
-    const ai = new GoogleGenAI({
-      apiKey: import.meta.env.VITE_GEMINI_API_KEY,
-    });
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: `Generate a response for the following prompt built using the Pentagram Framework for prompt engineering: Persona:{${formData.persona}}, Context:{${formData.context}}, Task:{${formData.task}}, Output:{${formData.output}}, Constraint:{${formData.constraint}}.`,
-    });
-    return response.text;
+    
+    setIsLoading(true);
+    try {
+      const response = await generateWithGemini(formData); // we can change this logic later to dinamicly select the AI model
+      setAiResponse(response);
+    } catch (error) {
+      console.error("Error fetching AI response:", error);
+      setAiResponse("**Error:** Unable to fetch response");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -109,16 +106,7 @@ function App() {
               ),
             )}
         </div>
-        {isSubmitted && (
-        <AiService
-          apiHandler={geminiApiHandler}
-          formData={formData}
-          isSubmitted={isSubmitted}
-          setIsSubmitted={setIsSubmitted}
-          setResponse={setAiResponse}
-        />
-      )}
-      {aiResponse && <OutputField response={aiResponse} />}
+        {isLoading ? (<OutputField response={'Loading...'} />) : (aiResponse && <OutputField response={aiResponse} />)}
         <div className="relative mx-1 mt-5 p-1">
           <TextArea
             title={currentStep.name}
