cffc43ccd85d5a7c589503b1ac24edabb48e6528
Greg <gregminezzi@gmail.com>
2025-04-04 11:25:21 +0200

    generate button pulses

--- client/src/components/GenerateButton.jsx
+++ client/src/components/GenerateButton.jsx
@@ -10,16 +10,16 @@ export default function GenerateButton({ formData }) {
       className="fixed right-3 bottom-3 z-100 flex cursor-pointer items-center justify-center hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"
     >
       <div
-        className={`absolute z-10 ${allValues ? "text-lg leading-[14px]" : "text-xs leading-[10px]"} text-white transition-all duration-500`}
+        className={`absolute z-10 ${allValues ? "text-lg leading-[14px] animate-grow-text" : "text-xs leading-[10px]"} text-white transition-all duration-500`}
       >
         <div>gen</div>
         <div>er</div>ate
       </div>
       <div
-        className={`penta animate-rotate absolute h-13 w-13 bg-black ${allValues && "h-17 w-17"} transition-all duration-500`}
+        className={`penta animate-rotate absolute h-13 w-13 bg-black ${allValues && "h-17 w-17 animate-grow "} transition-all duration-500`}
       ></div>
       <div
-        className={`penta animate-rotate relative h-12 w-12 bg-green-500 ${allValues && "h-16 w-16"} transition-all duration-500`}
+        className={`penta animate-rotate relative h-12 w-12 bg-green-500 ${allValues && "h-16 w-16 animate-grow "} transition-all duration-500`}
       ></div>
     </button>
   );
