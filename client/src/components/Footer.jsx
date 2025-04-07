80df2060cb6fb4462a817102fd153bb8b145d915
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-04-07 15:36:25 -0300

    Change footer links text color to black (contrast - accessibility)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,19 +6,19 @@ export default function Footer() {
       <div className="grid grid-cols-2">
         <ul className="list-none">
           <li>
-            <a href="#" className="text-white-text">
+            <a href="#" className="text-dark-text font-semibold">
               Anita Boakye-Yiadom
             </a>
             - Scrum Master
           </li>
           <li>
-            <a href="" className="text-white-text">
+            <a href="" className="text-dark-text font-semibold">
               Anas Maddah
             </a>
             - Developer
           </li>
           <li>
-            <a href="" className="text-white-text">
+            <a href="" className="text-dark-text font-semibold">
               Greg Minezzi
             </a>
             - Developer
@@ -26,26 +26,26 @@ export default function Footer() {
         </ul>
         <ul className="list-none">
           <li>
-            <a href="" className="text-white-text">
+            <a href="" className="text-dark-text font-semibold">
               Pat Okwu
             </a>
             - Product Owner
           </li>
           <li>
-            <a href="" className="text-white-text">
+            <a href="" className="text-dark-text font-semibold">
               Rafael Vecchi
             </a>
             - Developer
           </li>
           <li>
-            <a href="" className="text-white-text">
+            <a href="" className="text-dark-text font-semibold">
               Abdulsamad Yusuf
             </a>
             - Developer
           </li>
         </ul>
       </div>
-      <div className="mt-3"><a href="https://github.com/chingu-voyages/V54-tier3-team-34" className="text-white-text font-bold">GitHub </a>© 2025 Team 54-3-34. All rights reserved. </div>
+      <div className="mt-3"><a href="https://github.com/chingu-voyages/V54-tier3-team-34" className="text-dark-text font-semibold">GitHub </a>© 2025 Team 54-3-34. All rights reserved. </div>
     </div>
   );
 }
