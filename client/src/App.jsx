b0ac6769a0e646b056639733525c36786dd6aeac
Greg <gregminezzi@gmail.com>
2025-04-07 14:31:58 +0200

    remove secondary setLoading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,6 @@ import OutputField from "./components/OutputField";
 import { steps } from "./steps";
 import { generateWithGemini } from "./ai-model";
 
-
 function App() {
   const [formData, setFormData] = useState({
     persona: "",
@@ -23,8 +22,6 @@ function App() {
   const [errorMessages, setErrorMessages] = useState({});
   const [aiResponse, setAiResponse] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-
 
   // change currentStep everytime stepNumber changes
   useEffect(() => {
@@ -57,7 +54,7 @@ function App() {
 
   // check each key and see if there is value, create error message. if all filled out, return empty string
   const validateInput = () => {
-    setErrorMessages({})
+    setErrorMessages({});
     const missingData = {};
     // if step isn't filled out, create error object with key and error message
     Object.entries(formData).forEach(([key, value], index) => {
@@ -72,7 +69,6 @@ function App() {
   // handle submit form
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
 
     //check to make sure all data is present:
     const missingData = validateInput();
@@ -82,8 +78,6 @@ function App() {
     }
 
     // if all data is present, make api call
-
-// if all data is present, make api call
     setIsLoading(true);
     try {
       const response = await generateWithGemini(formData); // we can change this logic later to dinamicly select the AI model
@@ -96,12 +90,11 @@ function App() {
     }
   };
 
-
   return (
     <div className="flex min-h-screen flex-col items-center gap-5 bg-black">
       <Header />
       <div className="w-full max-w-[1000px] flex-1 border-green-500">
-        <div className="min-h-24 bg-green-500 p-3 m-2">
+        <div className="m-2 min-h-24 bg-green-500 p-3">
           {currentStep.description
             .split(" ")
             .map((word, i) =>
@@ -113,7 +106,11 @@ function App() {
             )}
         </div>
         {/* if there is output */}
-        {isLoading ? (<OutputField response={'Loading...'} />) : (aiResponse && <OutputField response={aiResponse} />)}
+        {isLoading ? (
+          <OutputField response={"Loading..."} />
+        ) : (
+          aiResponse && <OutputField response={aiResponse} />
+        )}
         <form onSubmit={handleSubmit} noValidate>
           <div className="relative mx-1 mt-5 p-1">
             <TextArea
@@ -124,16 +121,20 @@ function App() {
               handleKeyDown={handleKeyDown}
             />
             <div className="absolute bottom-5 flex w-full items-center justify-around gap-3">
-              <Button text="clear" onClick={handleClear} name={currentStep.name} />
+              <Button
+                text="clear"
+                onClick={handleClear}
+                name={currentStep.name}
+              />
               <Button
                 text="continue"
                 onClick={handleContinue}
                 name={currentStep.name}
                 disabled={stepNumber === 4}
               />
-           </div>
+            </div>
           </div>
-          <GenerateButton formData={formData}/>
+          <GenerateButton formData={formData} />
         </form>
         <ProgressBar
           steps={steps}
