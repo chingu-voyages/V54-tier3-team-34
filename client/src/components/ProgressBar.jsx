6446ab33f6537853c63d21af70591a8d8b387e3a
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-04-07 17:46:31 -0300

    make buttons more accessible (modify sizes and colors)

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export default function ProgressBar({ steps, stepNumber, setStepNumber, formData, errorMessages}) {
+export default function ProgressBar({
+  steps,
+  stepNumber,
+  setStepNumber,
+  formData,
+  errorMessages,
+}) {
   // move steps back
   const handleBack = () => {
     if (stepNumber === 0) return;
@@ -20,9 +26,9 @@ export default function ProgressBar({ steps, stepNumber, setStepNumber, formData
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex w-full items-center justify-center gap-5 text-4xl text-primary-green">
+      <div className="text-primary-green flex w-full items-center justify-center gap-5 text-5xl">
         <span
-          className={`mt-2 cursor-pointer text-2xl hover:text-white-text ${!stepNumber && "invisible"}`}
+          className={`hover:text-white-text cursor-pointer text-4xl ${!stepNumber && "invisible"}`}
           onClick={handleBack}
         >
           ←
@@ -33,11 +39,13 @@ export default function ProgressBar({ steps, stepNumber, setStepNumber, formData
             return (
               <div
                 key={i}
-                className="translate-y-[1.5px] cursor-pointer text-2xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"
+                className="-translate-y-1 duration-300 relative text-accent-green cursor-pointer text-5xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"
                 onClick={() => handleClick(i)}
               >
-                <div>⬟</div>
-                <div className="absolute top-[10px] left-[8px] text-xs font-bold text-dark-text">
+                {formData[step.name] ? <div className="text-accent-green absolute z-0">⬟</div> : null }
+                <div className="relative z-10">⬠</div>
+                {/* <div className="absolute top-[10px] left-[8px] text-xs font-bold text-white-text"> */}
+                <div className={`absolute top-[16px] left-[17px] text-xs font-bold z-50 ${formData[step.name] ? "text-dark-green-shade" : "text-accent-green"}`}>
                   {i + 1}
                 </div>
               </div>
@@ -47,11 +55,11 @@ export default function ProgressBar({ steps, stepNumber, setStepNumber, formData
             return (
               <div
                 key={i}
-                className="translate-y-[1.5px] cursor-pointer text-2xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"
+                className="relative cursor-pointer text-5xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"
                 onClick={() => handleClick(i)}
               >
-                <div className="text-primary-green/60">⬟</div>
-                <div className="absolute top-[10px] left-[8px] text-xs font-bold text-dark-text">
+                <div className="text-primary-green-darker-shade">⬟</div>
+                <div className="text-dark-text absolute top-[16px] left-[17px] text-xs font-bold z-50">
                   {i + 1}
                 </div>
               </div>
@@ -65,24 +73,28 @@ export default function ProgressBar({ steps, stepNumber, setStepNumber, formData
                 onClick={() => handleClick(i)}
               >
                 <div
-                  className={`${!!errorMessages?.[step.name] ? "text-red-500" : ""}`}
+                  className={`${errorMessages?.[step.name] ? "text-red-500" : "text-primary-green-darker-shade"}`}
                 >
                   ⬠
                 </div>
-                <div className="absolute top-[16px] left-[9px] text-xs text-white-text opacity-0 transition-all group-hover:opacity-100">
+                <div className="text-primary-green absolute top-[16px] left-[17px] text-xs opacity-100 group-hover:opacity-100 z-50">
                   {i + 1}
                 </div>
               </div>
             );
         })}
         <span
-          className={`mt-2 cursor-pointer text-2xl hover:text-white-text ${stepNumber === 4 && "invisible"}`}
+          className={`hover:text-white-text cursor-pointer text-4xl ${stepNumber === 4 && "invisible"}`}
           onClick={handleForward}
         >
           →
         </span>
       </div>
-        <div className="text-red-500 flex text-xs flex-col justify-center items-center">{Object.values(errorMessages).map(error => <div>{error}</div>)}</div>
+      <div className="flex flex-col items-center justify-center text-xs text-red-500">
+        {Object.values(errorMessages).map((error) => (
+          <div>{error}</div>
+        ))}
+      </div>
     </div>
   );
 }
