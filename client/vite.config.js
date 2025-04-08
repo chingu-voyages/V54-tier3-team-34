d7b46611c6352defc10b73512a5b80cbf8a5dd22
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-04-08 22:05:49 +0100

    build(client): proxy api requests to server's localhost on dev environments

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import process from "node:process";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(function ({ mode }) {
+  const devConfig = {};
+
+  if (mode == "development") {
+    const { SERVER_PORT } = loadEnv(mode, process.cwd(), "");
+    devConfig.server = {
+      proxy: { "/api/v1": { target: `http://localhost:${SERVER_PORT}/` } },
+    };
+  }
+
+  return { plugins: [react(), tailwindcss()], ...devConfig };
 });
