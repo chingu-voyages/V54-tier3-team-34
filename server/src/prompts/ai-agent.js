d762ca701af01f5bb5e3917fd22d0784eadaffab
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-04-08 20:40:28 +0100

    feat(prompt): improve prompt text

--- server/src/prompts/ai-agent.js
+++ server/src/prompts/ai-agent.js
@@ -6,8 +6,13 @@ function answerWithGemini({ constraint, context, format, persona, task }) {
   const gemini = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
   const prompt =
-    "Complete the following task:" +
-    JSON.stringify({ constraint, context, format, persona, task });
+    "Complete the following task as described below: \n{\n" +
+    `  constraint: ${constraint}\n` +
+    `  context: ${context}\n` +
+    `  format: ${format}\n` +
+    `  persona: ${persona}\n` +
+    `  task: ${task}\n` +
+    `}`;
 
   return gemini.models
     .generateContent({ model: "gemini-2.0-flash", contents: prompt })
